60dab49cd8b9d24ed15ad577e8c4f4a70be59926
Tajveez <Tajveez786@gmail.com>
2020-12-18 22:23:29 +0500

    More learning

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { toUnicode } from "punycode";
-
+	import Header from "./components/Header.svelte";
 	export let name;
+
 	let src = "https://svelte.dev/tutorial/image.gif";
 </script>
 
@@ -13,22 +13,21 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
+
+	p {
+		color: green;
+		font-family: "Comic Sans MS", cursive;
+		font-size: 2em;
+	}
 </style>
 
 <main>
-	<h1>Hello {name.length}!</h1>
+	<Header {name} />
 	<img {src} alt="Image of {name}" />
-	<!-- <p>This for {name}</p> -->
+	<p>{name} has length of {name.length}</p>
 </main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    let name = "Not Imported";
+</script>
+
+<style>
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
+
+<main>
+    <h1>Hello {name}!</h1>
+</main>
